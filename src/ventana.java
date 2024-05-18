97316c2a3268486d56e380ed0957fae21ac6ab58
David_Calvo <david.calvo@mataro.epiaedu.cat>
2024-05-18 11:36:33 +0200

    first commit, just a simple window

---
+++ src/ventana.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class ventana extends JFrame {
+    public ventana() {
+        this.setSize(400, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Calculadora");
+        this.setLocationRelativeTo(null);
+    }
+
+
+}
